e3aecbcb14005391e47fd35298dc901ff9916c66
Dmitriy Kostarev <dmitriykostarev9987@gmail.com>
2024-03-23 17:28:56 +0300

    add model.py, add db.py, add loader for point_table

---
+++ db_utils/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer
+from sqlalchemy import DateTime, JSON
+from sqlalchemy.sql import func
+
+from db import Base, engine
+
+
+class PointTable(Base):
+    __tablename__ = 'point_table'
+    id = Column(Integer, primary_key=True)
+    data = Column(JSON)
+    create_date = Column(DateTime(timezone=True), server_default=func.now())
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
