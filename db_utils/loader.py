54326346ecf26e493ea018718748ccab5eea68cb
Dmitriy Kostarev <dmitriykostarev9987@gmail.com>
2024-03-25 17:41:09 +0300

    add cron files; fix imports; refresh .png point_table from database

--- db_utils/loader.py
+++ db_utils/loader.py
@@ -1,8 +1,14 @@
-from db import db_session
-from models import PointTable
+from epl_bot.db_utils.db import db_session
+from epl_bot.db_utils.models import PointTable
 from epl_bot.parser.standings import get_league_standings
 
-new_string = PointTable(data=get_league_standings(league=39, season=2023))
 
-db_session.add(new_string)
-db_session.commit()
+def load_string_point_table():
+    new_string = PointTable(data=get_league_standings(league=39, season=2023))
+
+    db_session.add(new_string)
+    db_session.commit()
+
+
+if __name__ == '__main__':
+    load_string_point_table()
